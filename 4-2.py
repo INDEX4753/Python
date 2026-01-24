d398913180465ec905dd50fd5b059e1d6237c251
INDEX4753 <205864343@qq.com>
2026-01-24 19:32:56 +0800

    test git

--- 4-2.py
+++ 4-2.py
@@ -1,20 +1,3 @@
 """
 4-2
 """
-
-def main():
-    n = int(input())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-
-    a.sort()
-    b.sort()
-
-    ans = 0
-    for i in range(n):
-        ans += abs(a[i] - b[i])
-
-    print(ans)
-
-if __name__ == "__main__":
-    main()
